c500323fee5bd0f655e4bf88fa68cf9f7e1d052d
Debayan Pratihar <debayanpratihar1234@gmail.com>
2023-07-27 21:49:57 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -94,7 +94,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."   (Turn on the loader)
     loader(messageDiv)
 //Fetch the data from the server -> bot's responce
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://dr-debayanai.onrender.com', {
         method: 'POST',        //Object containing all of the objects
         headers: {
             'Content-Type': 'application/json',
